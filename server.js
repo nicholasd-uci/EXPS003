6f3e0a97714e5534fe820c2273944061da5a8a14
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-25 14:42:04 -0800

    testing server

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 // Express & Path
-const express = require('require')
-const { join } = require('path')
+// const express = require('require')
+// const { join } = require('path')
 
 // App
-const app = express()
+// const app = express()
 
 // Connects the Front-End
-app.use(express.static(join(__dirname, 'public')))
+// app.use(express.static(join(__dirname, 'public')))
 
 // Middleware - send/receive json data
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
 
 // we moved this section to itemRoutes.js
-app.use(require('./routes'))
+// app.use(require('./routes'))
 
 
 // PORT
+// app.listen(3000)
+
+
+
+const express = require('express')
+
+const app = express()
+
+app.get('/name', (req, res) => {
+    console.log(req.query)
+    res.send('hi')
+})
+
 app.listen(3000)
+
 
 
 
