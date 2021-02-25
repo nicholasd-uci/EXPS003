cbc420f491bde4a381950ca25b8560250fae8df4
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-25 14:25:11 -0800

    cleaned up notes "easy to read" - 200

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-// Express
+// Express & Path
 const express = require('require')
-
-// this will help your frontend view routing to render with 1 line of code
-// need a public folder
-// file inside of that needs to be index.html
 const { join } = require('path')
 
 // App
 const app = express()
 
-
-// this is compatable with line const { join }
-// this hooks up your front end!
+// Connects the Front-End
 app.use(express.static(join(__dirname, 'public')))
 
-// middleware - steps in the middle of your frontend - backend (request spot)
-// we use this to say that we accept JSON Data - this lets it send and receive Data (SAME EVERYTIME === BoilerPlate)
-// C&P lines 3 - 27 everytime setting up a server side // 
+// Middleware - send/receive json data
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 // we moved this section to itemRoutes.js
 app.use(require('./routes'))
-// we just called this app.use from line 9 to link our text that we had here to now render here when its called upon! 
-// we moved this section to itemRoutes.js
+
 
 // PORT
 app.listen(3000)
+
+
 
 // npm init -y
 // npm i express
