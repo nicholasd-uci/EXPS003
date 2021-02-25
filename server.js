c698f1c6ce5db52d30d231b591a817af17c5f80e
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-25 14:58:23 -0800

    tested query in URL

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const express = require('express')
 const app = express()
 
 app.get('/name', (req, res) => {
-    console.log(req.query)
-    res.send('hi')
+    console.log(req.query.n)
+    res.send(`Hello ${req.query.n}`)
 })
 
 app.listen(3000)
