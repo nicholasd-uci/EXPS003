a1458649be17a6ca316a9e1a9a830097313ceb67
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-27 15:22:21 -0800

    params status: 200

--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 // Express & Path
-// const express = require('require')
-// const { join } = require('path')
+const express = require('require')
+const { join } = require('path')
 
 // App
-// const app = express()
+const app = express()
 
 // Connects the Front-End
-// app.use(express.static(join(__dirname, 'public')))
+app.use(express.static(join(__dirname, 'public')))
 
 // Middleware - send/receive json data
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // we moved this section to itemRoutes.js
-// app.use(require('./routes'))
+app.use(require('./routes'))
 
 
 // PORT
-// app.listen(3000)
-
-
+app.listen(3000)
 
 const express = require('express')
 const app = express()
@@ -30,9 +28,6 @@ app.get('/name/:n', (req, res) => {
 })
 
 app.listen(3000)
-
-
-
 
 // npm init -y
 // npm i express
