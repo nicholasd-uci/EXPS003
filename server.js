5b0276899e9011bf3ed6168324cef18a93bc29e4
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-27 15:21:38 -0800

    tested params

--- server.js
+++ server.js
@@ -22,12 +22,11 @@
 
 
 const express = require('express')
-
 const app = express()
 
-app.get('/name', (req, res) => {
-    console.log(req.query.n)
-    res.send(`Hello ${req.query.n}`)
+app.get('/name/:n', (req, res) => {
+    console.log(req.params.n)
+    res.send(`Hello ${req.params.n}!`)
 })
 
 app.listen(3000)
