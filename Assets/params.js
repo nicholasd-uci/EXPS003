49abd2fdd09cf445d56ec171c623f41bca9a4403
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-27 18:10:51 -0800

    created asset forlder for calc ex.

--- Assets/params.js
+++ Assets/params.js
@@ -1,17 +1,7 @@
 const express = require('express')
 ​
 const app = express()
-​
-// app.get('/name', (req, res) => {
-//   console.log(req.query.n)
-//   res.send(`Hello ${req.query.n}!`)
-// })
-​
-// app.get('/name/:n/:x/:y', (req, res) => {
-//   console.log(req.params)
-//   res.send(`Hello ${req.params.n}!`)
-// })
-​
+​​
 app.get('/calc/:op/:a/:b', (req, res) => {
 ​
   let a = parseInt(req.params.a)
