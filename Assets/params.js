36679903f7380e14bf350e025759c73b8a450cee
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-28 11:00:29 -0800

    commented out params.js

--- Assets/params.js
+++ Assets/params.js
@@ -1,30 +1,30 @@
-const express = require('express')
-​
-const app = express()
-​​
-app.get('/calc/:op/:a/:b', (req, res) => {
-​
-  let a = parseInt(req.params.a)
-  let b = parseInt(req.params.b)
-​
-  switch (req.params.op) {
-    case 'add':
-      res.send(`${a + b}`)
-      break
-    case 'subtract':
-      res.send(`${a - b}`)
-      break
-    case 'multiply':
-      res.send(`${a * b}`)
-      break
-    case 'divide':
-      res.send(`${a / b}`)
-      break
-  }
-})
-​
-app.listen(3000)
-Collapse
+// const express = require('express')
+// ​
+// const app = express()
+// ​​
+// app.get('/calc/:op/:a/:b', (req, res) => {
+// ​
+//   let a = parseInt(req.params.a)
+//   let b = parseInt(req.params.b)
+// ​
+//   switch (req.params.op) {
+//     case 'add':
+//       res.send(`${a + b}`)
+//       break
+//     case 'subtract':
+//       res.send(`${a - b}`)
+//       break
+//     case 'multiply':
+//       res.send(`${a * b}`)
+//       break
+//     case 'divide':
+//       res.send(`${a / b}`)
+//       break
+//   }
+// })
+// ​
+// app.listen(3000)
+// Collapse
 
 
 
