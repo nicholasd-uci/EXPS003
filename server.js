27d3be188acb455b2aa3b7df7697e1e3029f2e53
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-28 21:08:50 -0800

    updated nodemon

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+let items = []
 
 // GET all item
 app.get('/items', (req, res) => {
-
+    res.json(items)
 })
 
 // POST one item
@@ -20,7 +21,7 @@ app.post('/items', (req, res) => {
 
 // PUT one item
 app.put('/items/:id', (req, res) => {
-    
+
 })
 
 // DELETE one item
