4c00672df99c5f72cfcc76aa4d3a0cc93de75bdd
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-03-01 10:29:18 -0800

    required express router

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,18 +1,19 @@
+const router = require('express').Router()
 let items = []
 
 // GET all item
-app.get('/items', (req, res) => {
+router.get('/items', (req, res) => {
     res.json(items)
 })
 
 // POST one item
-app.post('/items', (req, res) => {
+router.post('/items', (req, res) => {
     items.push(req.body)
     res.sendStatus(200)
 })
 
 // PUT one item
-app.put('/items/:text', (req, res) => {
+router.put('/items/:text', (req, res) => {
     for (let i = 0; i < items.length; i++) {
         if (items[i].text === req.params.text) {
             items[i].isDone = req.body.isDone
@@ -22,8 +23,10 @@ app.put('/items/:text', (req, res) => {
 })
 
 // DELETE one item
-app.delete('/items/:text', (req, res) => {
+router.delete('/items/:text', (req, res) => {
     items = items.filter(item => item.text !== req.params.text)
     res.sendStatus(200)
 
 })
+
+module.exports = router
