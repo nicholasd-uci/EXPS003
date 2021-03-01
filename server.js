bf5dcfa16f76643803a57b90c260543b806e47d9
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-02-28 20:18:57 -0800

    wrote our RESTful syntax for rountes

--- server.js
+++ server.js
@@ -7,4 +7,25 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
+
+// GET all item
+app.get('/items', (req, res) => {
+
+})
+
+// POST one item
+app.post('/items', (req, res) => {
+
+})
+
+// PUT one item
+app.put('/items/:id', (req, res) => {
+    
+})
+
+// DELETE one item
+app.delete('/items/:id', (req, res) => {
+
+})
+
 app.listen(3000)
