7cb1447a5393700b073d6bb5127c53acc9489987
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-03-01 10:26:34 -0800

    removed routes from server.js and moved to routes folder

---
+++ routes/itemRoutes.js
@@ -0,0 +1,29 @@
+let items = []
+
+// GET all item
+app.get('/items', (req, res) => {
+    res.json(items)
+})
+
+// POST one item
+app.post('/items', (req, res) => {
+    items.push(req.body)
+    res.sendStatus(200)
+})
+
+// PUT one item
+app.put('/items/:text', (req, res) => {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].text === req.params.text) {
+            items[i].isDone = req.body.isDone
+        }
+    }
+    res.sendStatus(200)
+})
+
+// DELETE one item
+app.delete('/items/:text', (req, res) => {
+    items = items.filter(item => item.text !== req.params.text)
+    res.sendStatus(200)
+
+})
--- server.js
+++ server.js
@@ -7,34 +7,4 @@ app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
-let items = []
-
-// GET all item
-app.get('/items', (req, res) => {
-    res.json(items)
-})
-
-// POST one item
-app.post('/items', (req, res) => {
-    items.push(req.body)
-    res.sendStatus(200)
-})
-
-// PUT one item
-app.put('/items/:text', (req, res) => {
-    for (let i = 0; i < items.length; i++) {
-        if (items[i].text === req.params.text) {
-            items[i].isDone = req.body.isDone
-        }
-    }
-    res.sendStatus(200)
-})
-
-// DELETE one item
-app.delete('/items/:text', (req, res) => {
-    items = items.filter(item => item.text !== req.params.text)
-    res.sendStatus(200)
-
-})
-
 app.listen(3000)
