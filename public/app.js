baff30fee3e80b464aea77c3e2cebb2151490149
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-03-25 11:31:51 -0700

    included axios request

--- public/app.js
+++ public/app.js
@@ -1,6 +1,9 @@
+
+axios.get('/api/items')
+.then()
+.catch(err => console.error(err))
+
 document.getElementById('addItem').addEventListener('click', event => {
     event.preventDefault()
 
-
-    
 })
